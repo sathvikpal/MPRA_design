4fc6c85833173e09dda1abd5cd279225bbd6e37d
Sathvik Palakurty <sathvik@htcf.htcf>
2020-04-02 01:16:01 -0500

    changed relative directory to absolute

--- atac_pipe/submitJSON.sh
+++ atac_pipe/submitJSON.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-tmpfile=$(mktemp)
+tmpfile="$(mktemp)"
 
-r1="../../rawdata/fastq/${1}_1.fastq.gz"
-r2="../../rawdata/fastq/${1}_2.fastq.gz"
 mkdir -p "../../data/atac_output/${1}"
+
+r1=`cd ../../rawdata/fastq/${1}_1.fastq.gz; pwd`
+r2=`cd ../../rawdata/fastq/${1}_2.fastq.gz; pwd`
 
 echo '{
     "atac.title" : "'$1'",
@@ -18,14 +19,17 @@ echo '{
     "atac.paired_end" : true,
 
     "atac.fastqs_rep1_R1" : [ "'${r1}'" ],
-    "atac.fastqs_rep1_R2" : [ "'${r2}" ],
+    "atac.fastqs_rep1_R2" : [ "'${r2}'" ],
 
     "atac.auto_detect_adapter" : true,
 
     "atac.multimapping" : 4
 }' > ${tmpfile}
 
-cd "../../data/atac_ouput/${1}"
-conda activate encode-atac-seq-pipeline 
-sbatch -J "${1}" --export=ALL --mem 3G -t 4-0 --wrap "caper run ~/atac-seq-pipeline/atac.wdl -i '${tmpfile}'"
+cd "../../data/atac_output/${1}"
+
+conda init bash
+conda activate encode-atac-seq-pipeline
+
+caper run ~/atac-seq-pipeline/atac.wdl -i ${tmpfile}
 rm ${tmpfile}
